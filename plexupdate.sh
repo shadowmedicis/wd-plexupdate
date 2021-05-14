1457cdec5f51121e343b550fe158a02cc1a9a3d3
shadowmedicis <80291786+shadowmedicis@users.noreply.github.com>
2021-05-14 17:19:30 +0200

    Update plexupdate.sh

--- plexupdate.sh
+++ plexupdate.sh
@@ -8,10 +8,10 @@
 
 url_plexapi="https://plex.tv/api/downloads/5.json"
 
-newversion=$(curl -s $url_plexapi | sed -n 's|.*"version":"\([^"]*\)-.*|\1|p')
-echo "Latest version: $newversion"
+newversion=$(curl -s "$url_plexapi" | sed -n 's|.*"version":"\([^"]*\)-.*|\1|p')
+echo "Latest version: "$newversion""
 curversion=$(cat /shares/Volume_1/Nas_Prog/plexmediaserver/apkg.rc | grep "Version:" | cut -d ":" -f 2 | sed 's/^ *//g')
-echo "Current version: $curversion"
+echo "Current version: "$curversion""
 if [[ "$newversion" > "$curversion" ]]
     then
     echo "Updated Version of Plex is available. Starting download & install."
@@ -27,11 +27,11 @@ if [[ "$newversion" > "$curversion" ]]
 	osmodel=""$model".bin"
     fi
 
-    url=$(curl -s $url_plexapi | python3 -m json.tool | grep $osmodel | cut -d '"' -f 4)
+    url=$(curl -s "$url_plexapi" | python3 -m json.tool | grep "$osmodel" | cut -d '"' -f 4)
     wget --no-check-certificate "$url" -P /shares/Volume_1/.systemfile/upload
 
-    filename=$(echo $url | cut -d "/" -f 7)
-    /usr/sbin/upload_apkg -r$filename -d -f1 -g1
+    filename=$(echo "$url" | cut -d "/" -f 7)
+    /usr/sbin/upload_apkg -r"$filename" -d -f1 -g1
 else
     echo "No new version available"
 fi
