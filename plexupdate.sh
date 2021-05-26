356849c656358937a224b5c9689bfa32c212a464
shadowmedicis <80291786+shadowmedicis@users.noreply.github.com>
2021-05-26 16:21:38 +0200

    Add files via upload

--- plexupdate.sh
+++ plexupdate.sh
@@ -13,24 +13,25 @@ echo "Latest version: "$newversion""
 curversion=$(cat /shares/Volume_1/Nas_Prog/plexmediaserver/apkg.rc | grep "Version:" | cut -d ":" -f 2 | sed 's/^ *//g')
 echo "Current version: "$curversion""
 if [[ "$newversion" > "$curversion" ]]
-    then
+then
     echo "Updated Version of Plex is available. Starting download & install."
 
-    model=$(cat /usr/local/config/config.xml | grep "<hw_ver>" | sed -e 's/\(<[^<][^<]*>\)//g' | cut -d "." -f 1)
-    OS_majorversion=$(cat /usr/local/config/config.xml | grep "<sw_ver_1>" | sed -e 's/\(<[^<][^<]*>\)//g' | cut -d "." -f 1)
-
-    if [["OS_majorversion" = "5" ]]
-	then
+    model="$(cat /usr/local/config/config.xml | grep "<hw_ver>" | sed -e 's/\(<[^<][^<]*>\)//g' | cut -d "." -f 1)"
+    OS_majorversion="$(cat /usr/local/config/config.xml | grep "<sw_ver_1>" | sed -e 's/\(<[^<][^<]*>\)//g' | cut -d "." -f 1)"
+    
+    if [[ "$OS_majorversion" -eq 5 ]]
+    then
 	osmodel=""$model"_OS5.bin"
 		
     else
 	osmodel=""$model".bin"
     fi
 
-    url=$(curl -s "$url_plexapi" | python3 -m json.tool | grep "$osmodel" | cut -d '"' -f 4)
+    url=$(curl -s "$url_plexapi" | python3 -m json.tool | grep $osmodel | cut -d '"' -f 4)
     wget --no-check-certificate "$url" -P /shares/Volume_1/.systemfile/upload
-
+    
     filename=$(echo "$url" | cut -d "/" -f 7)
+    
     /usr/sbin/upload_apkg -r"$filename" -d -f1 -g1
 else
     echo "No new version available"
